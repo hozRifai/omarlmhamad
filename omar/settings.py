50cfb82ee7f6e09cfed2219f7ef999c137d089d3
Houzayfa Rifai <p-p-h@hotmail.com>
2017-09-04 12:05:06 +0400

    ds

--- omar/settings.py
+++ omar/settings.py
@@ -28,7 +28,7 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://omarlmhamad.herokuapp.com/' , "omarlmhamad.herokuapp.com"]
 
 DEBUG = False
 # Application definition
