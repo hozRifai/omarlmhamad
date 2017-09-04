63ad5a97e64a643d4e60031f1cdd6f55762c9008
Houzayfa Rifai <p-p-h@hotmail.com>
2017-09-04 11:51:50 +0400

    hey

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web : gunicorn omar.wsgi 
+web: gunicorn omar.wsgi --log-file - 
--- omar/wsgi.py
+++ omar/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omarlmhamad.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omar.settings")
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
 
 application = get_wsgi_application()
