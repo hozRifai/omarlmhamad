a3ef8bf88bdb9cd180b8757508d716caf712c000
Houzayfa Rifai <p-p-h@hotmail.com>
2017-09-04 18:06:31 +0400

    Ds

--- omar/settings.py
+++ omar/settings.py
@@ -153,5 +153,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
+MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
