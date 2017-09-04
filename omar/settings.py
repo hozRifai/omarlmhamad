7ca45c19edb3477074df9de2eaf6711d7ce5dd08
Houzayfa Rifai <p-p-h@hotmail.com>
2017-09-04 18:22:52 +0400

    Ds

--- omar/settings.py
+++ omar/settings.py
@@ -146,13 +146,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
  
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
+STATIC_ROOT = 'static'
--- omar/urls.py
+++ omar/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url , include
 from django.contrib import admin
-from . import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include("users.urls")),
-]   
-urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
