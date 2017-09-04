359d936a10c23b8b81ea86a4dec100f0c06b4a10
Houzayfa Rifai <p-p-h@hotmail.com>
2017-09-04 16:22:31 +0400

    ds

--- omar/settings.py
+++ omar/settings.py
@@ -84,10 +84,14 @@ WSGI_APPLICATION = 'omar.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
